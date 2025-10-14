af93cc68974345a4defbad889cd43ac954abb6ca
Ralf Comtesse <rcc@ralf-comtesse.de>
2025-10-14 13:07:30 +0200

    removed junit dependency from AuthenticationServiceTest.scala

--- scala/src/test/scala/com/pduda/method/AuthenticationServiceTest.scala
+++ scala/src/test/scala/com/pduda/method/AuthenticationServiceTest.scala
@@ -1,19 +1,17 @@
 package com.pduda.method
 
 import com.pduda.method.AuthenticationService
-import org.junit.Assert
-import org.junit.Test
 
 class AuthenticationServiceTest extends munit.FunSuite {
   test("administrator is always authenticated") {
     val service = new AuthenticationService
     val adminId = 12345
-    Assert.assertTrue(service.isAuthenticated(adminId))
+    assert(service.isAuthenticated(adminId))
   }
 
   test("normal user is not authenticated initially") {
     val service      = new AuthenticationService
     val normalUserId = 11111
-    Assert.assertFalse(service.isAuthenticated(normalUserId))
+    assert(!service.isAuthenticated(normalUserId))
   }
 }

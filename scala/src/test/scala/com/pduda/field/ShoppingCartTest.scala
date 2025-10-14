f1d2f5617cb3c831f33dc0ce9b337e859b39b34a
Ralf Comtesse <rcc@ralf-comtesse.de>
2025-10-14 21:21:47 +0200

    removed test dependency to junit.Assert

--- scala/src/test/scala/com/pduda/field/ShoppingCartTest.scala
+++ scala/src/test/scala/com/pduda/field/ShoppingCartTest.scala
@@ -1,29 +1,27 @@
 package com.pduda.field
 
-import org.junit.Assert
-
 class ShoppingCartTest extends munit.FunSuite {
   test("single item: number of products in the cart") {
     val shoppingCart = new ShoppingCart
     shoppingCart.add(10)
-    Assert.assertEquals(1, shoppingCart.numberOfProducts)
+    assertEquals(1, shoppingCart.numberOfProducts)
   }
 
   test("single item: total price") {
     val shoppingCart = new ShoppingCart
     shoppingCart.add(10)
-    Assert.assertEquals(10, shoppingCart.calculateTotalPrice)
+    assertEquals(10, shoppingCart.calculateTotalPrice)
   }
 
   test("single item: has discount if contains at least one product worth at least 100") {
     val shoppingCart = new ShoppingCart
     shoppingCart.add(100)
-    Assert.assertTrue(shoppingCart.hasDiscount)
+    assert(shoppingCart.hasDiscount)
   }
 
   test("single item: does not have discount if contains no products worth at least 100") {
     val shoppingCart = new ShoppingCart
     shoppingCart.add(99)
-    Assert.assertFalse(shoppingCart.hasDiscount)
+    assert(!shoppingCart.hasDiscount)
   }
 }
